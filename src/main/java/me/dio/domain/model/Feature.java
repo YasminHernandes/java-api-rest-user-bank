44a90c1aabfde5ab0f48684943ac103a9fc5f42f
yasminhernandes <yasminhernandesjobs@gmail.com>
2023-09-24 21:17:11 -0300

    feat: implement domain layer

---
+++ src/main/java/me/dio/domain/model/Feature.java
@@ -0,0 +1,8 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {}
